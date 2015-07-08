e0c5d0ec0e4ae7e13fb09c27fac1967041887bbf
pavlo520a <pavlo520a@gmail.com>
2015-07-08 20:54:54 +0300

    CrimeCIty - empty project added

---
+++ CrimeCity/CrimeCity.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CrimeCity.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [TestCase]
+        public void Test1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+
+        [TestCase]
+        public void FailedTest()
+        {
+            Assert.AreEqual(0, 1);
+        }
+    }
+}
---
+++ CrimeCity/CrimeCity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrimeCity.Startup))]
+namespace CrimeCity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
