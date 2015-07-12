8c5105ca18713f061739b0361ae6f93e110bc7d0
pavlo520a <pavlo520a@gmail.com>
2015-07-12 18:44:14 +0300

    Removed unused tests. Added logic to hash and verify password. Added tests for PasswordEncryptor.

--- CrimeCity/CrimeCity/BusinessLogic/PasswordEncryptor.cs
+++ CrimeCity/CrimeCity/BusinessLogic/PasswordEncryptor.cs
@@ -2,21 +2,39 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CrimeCity.BusinessLogic
 {
+    /// <summary>
+    /// This class will be needed to save password (in safe way) into database
+    /// and to check is entered by user password is correct
+    /// </summary>
     public class PasswordEncryptor : IPasswordEncryptor
     {
         public string GetHash(string password)
         {
-            throw new NotImplementedException();
+            return GetSHA1Hash(password);
         }
 
-        public bool IsValid(string password)
+        public bool IsValid(string password, string hash)
         {
-            throw new NotImplementedException();
+            return string.Compare(GetSHA1Hash(password), hash) == 0;
+        }
+
+        private string GetSHA1Hash(string password)
+        {
+            SHA1 sha1 = SHA1.Create();
+            StringBuilder returnValue = new StringBuilder();
+
+            byte[] hashData = sha1.ComputeHash(Encoding.Default.GetBytes(password));
+
+            for (int i = 0; i < hashData.Length; i++)
+                returnValue.Append(hashData[i].ToString("x2"));
+
+            return returnValue.ToString();
         }
     }
 }
