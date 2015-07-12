133301cad65c41d4be39fbb38d86e17077372250
Serhiy4884 <pohutsserhiy@gmail.com>
2015-07-13 02:11:56 +0300

    вав

---
+++ CrimeCity/CrimeCity/Models/Employees/Positions/Policeman.cs
@@ -0,0 +1,18 @@
+﻿using CrimeCity.Models.Crimes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrimeCity.Models.Employees.Positions
+{
+    public class Policeman : Employee
+    {
+        public Policeman()
+        {
+            this.Position = PolicePositions.Policeman;
+        }
+
+        public List<Crime> AssignedCrimes { get; set; }
+    }
+}
