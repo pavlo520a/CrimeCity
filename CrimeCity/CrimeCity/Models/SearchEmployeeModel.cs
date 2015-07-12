994dd41d3386c2a496032d32c40ab88912ec1ba0
pavlo520a <pavlo520a@gmail.com>
2015-07-12 19:53:50 +0300

    Search page for administrator. Needs refactoring. Should be extended.Button new does not work.

---
+++ CrimeCity/CrimeCity/Models/SearchEmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿using CrimeCity.Models.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrimeCity.Models
+{
+    public class SearchEmployeeModel
+    {
+        public SearchEmployeeCriteria SearchCriteria { get; set; }
+        public List<Employee> SearchResult { get; set; }
+
+        public bool IsNotEmpty
+        {
+            get { return SearchResult.Count > 0; }
+        }
+
+        public SearchEmployeeModel()
+        {
+            SearchCriteria = new SearchEmployeeCriteria();
+            SearchResult = new List<Employee>();
+        }
+    }
+}
